be252841d584bfe1c3a0665c7ae4b833f909cb6a
Matt Lindeboom <mattl@surveymonkey.com>
2020-03-02 21:56:03 -0800

    no layout

--- src/pages/projects/periodic.js
+++ src/pages/projects/periodic.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ProjectLayout from '../../components/Layout/ProjectLayout';
+import Helmet from 'react-helmet';
+import { withPrefix } from 'gatsby';
 
-export default ({ children }) => (
-  <ProjectLayout>
-  </ProjectLayout>
+export default () => (
 
+  <Helmet bodyAttributes={{
+    class: 'periodic-table'
+  }}>
+    <html lang="en" />
+    <title>Periodic Table</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1' />
+    <title>Periodic Table</title>
+    <link rel='stylesheet' href={withPrefix('pt/bundle.css')} />
+
+    <script defer src={withPrefix('pt/bundle.js')}></script>
+  </Helmet>
 );
