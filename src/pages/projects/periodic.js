eb336bb968db7b5a5b7513620629d18ee47f33cd
Matt Lindeboom <mattl@surveymonkey.com>
2020-03-03 14:47:31 -0800

    Purecomponent on page for shallow rerender

--- src/pages/projects/periodic.js
+++ src/pages/projects/periodic.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Helmet from 'react-helmet';
 import { withPrefix } from 'gatsby';
 import Layout from '../../components/Layout';
 
-export default () => (
-  <Layout>
-    <Helmet bodyAttributes={{
-      class: 'periodic-table'
-    }}>
-      <html lang="en" />
-      <title>Periodic Table</title>
-      <meta name='viewport' content='width=device-width,initial-scale=1' />
-      <title>Periodic Table</title>
-      <link rel='stylesheet' href={withPrefix('pt/bundle.css')} />
+export default class extends PureComponent {
+  render() {
+    return (
+      <Layout>
+        <Helmet bodyAttributes={{
+          class: 'periodic-table'
+        }}>
+          <html lang="en" />
+          <title>Periodic Table</title>
+          <meta name='viewport' content='width=device-width,initial-scale=1' />
+          <title>Periodic Table</title>
+          <link rel='stylesheet' href={withPrefix('pt/bundle.css')} />
 
-      <script defer src={withPrefix('pt/bundle.js')}></script>
-    </Helmet>
-    <div className='pt-container' ></div>
-  </Layout>
-);
+          <script defer src={withPrefix('pt/bundle.js')}></script>
+        </Helmet>
+        <div className='pt-container' ></div>
+      </Layout>
+    );
+  }
+}
